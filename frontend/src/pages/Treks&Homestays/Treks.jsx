34144edcd92f1583a354a987fb37a897f148d1d7
Harsh <harshjat12345671@gmail.com>
2024-10-10 18:37:56 +0530

    trek page taking data from database

--- frontend/src/pages/Treks&Homestays/Treks.jsx
+++ frontend/src/pages/Treks&Homestays/Treks.jsx
@@ -1,37 +1,39 @@
-import React from 'react';
-import './Treks.css';
+import React from "react";
+import "./Treks.css";
 import Sliders from "../../components/data_trek_homestays/treks_card_data.jsx";
 import Header from "../../components/common/Header/Header.jsx";
-import { track_items } from '../../assets/assets';
-import SecondBanner from '../../components/Banner/secondBanner.jsx';
+import { track_items } from "../../assets/assets";
+import SecondBanner from "../../components/Banner/secondBanner.jsx";
+import GetTrack from "../../components/GetTrack/GetTrack.jsx";
 
 const Treks = () => {
-    return (
-        <>
-            <Header />
-            <SecondBanner />
-            <section className='Treks-section'>
-                <div className="trek-allCards">
-                    {track_items.map((item, index) => {
-                        return (
-                            <Sliders
-                            key={index}
-                            id={item.id}
-                            name={item.name}
-                            duration={item.duration}
-                            difficulty={item.difficulty}
-                            real_price={item.real_price}
-                            discounted_price={item.discounted_price}
-                            image={item.image}
-                            difficulty_image={item.difficulty_image}
-                            dur_image={item.dur_image}
-                            />
-                        );
-                    })}
-                </div>
-            </section>
-        </>
-    )
-}
+  const treks = GetTrack();
+  return (
+    <>
+      <Header />
+      <SecondBanner />
+      <section className="Treks-section">
+        <div className="trek-allCards">
+          {treks.map((item, index) => {
+            return (
+              <Sliders
+                key={index}
+                id={item.id}
+                name={item.name}
+                duration={item.duration}
+                difficulty={item.difficulty}
+                real_price={item.realPrice}
+                discounted_price={item.discountedPrice}
+                image={item.image}
+                difficulty_image={track_items[0].difficulty_image}
+                dur_image={track_items[0].dur_image}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Treks;
